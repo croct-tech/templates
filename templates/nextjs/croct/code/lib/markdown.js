e4a31b6dd5ab79da613e1aa31688300be5db97e9
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-10 16:31:08 -0300

    Add templates for Next 13, 14 and 15 (#1)

---
+++ templates/nextjs/croct/code/lib/markdown.js
@@ -0,0 +1,16 @@
+import {render} from "@croct/md-lite"
+import Image from "next/image"
+
+export function renderMarkdown(content, classes = {}) {
+    return render(content, {
+        fragment: node => node.children,
+        text: node => node.content,
+        bold: node => <b className={classes.bold} key={node.index}>{node.children}</b>,
+        italic: node => <i className={classes.italic} key={node.index}>{node.children}</i>,
+        strike: node => <s className={classes.strike} key={node.index}>{node.children}</s>,
+        code: node => <code className={classes.code} key={node.index}>{node.content}</code>,
+        link: node => <a className={classes.link} key={node.index} href={node.href}>{node.children}</a>,
+        image: node => <Image key={node.index} src={node.src} alt={node.alt} />,
+        paragraph: node => <p className={classes.paragraph} key={node.index}>{node.children}</p>,
+    });
+}

1535d8411bbd946e701dfae759a0f23d3ff3766c
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-15 15:14:49 -0300

    Add template for Magic UI lens (#17)

---
+++ templates/magicui/ui/lens/code/components/lens-card.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from "?/**/*/ui/button.tsx";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "?/**/*/ui/card.tsx";
+import { Lens } from "?/**/*/magicui/lens.tsx";
+
+interface LensCardProps {
+  image: string;
+  zoomFactor?: number;
+  lensSize?: number;
+  lensColor?: string;
+  duration?: number;
+}
+
+export function LensCard({image, ...props}: LensCardProps) {
+  return (
+    <Card className="relative max-w-md shadow-none">
+      <CardHeader>
+        <Lens
+          zoomFactor={2}
+          lensSize={150}
+          isStatic={false}
+          ariaLabel="Zoom Area"
+          {...props}
+        >
+          <img
+            src={image}
+            alt="Thumbnail"
+            width={500}
+            height={500}
+          />
+        </Lens>
+      </CardHeader>
+      <CardContent>
+        <CardTitle className="text-2xl">Your next camp</CardTitle>
+        <CardDescription>
+          See our latest and best camp destinations all across the five
+          continents of the globe.
+        </CardDescription>
+      </CardContent>
+      <CardFooter className="space-x-4">
+        <Button>Let&apos;s go</Button>
+        <Button variant="secondary">Another time</Button>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ templates/magicui/ui/lens/code/page/nextjs/page.tsx
@@ -0,0 +1,24 @@
+import { LensCard } from "?/**/*/lens-card.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export default async function Page() {
+  const {content} = await fetchContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Lens"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <LensCard {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}

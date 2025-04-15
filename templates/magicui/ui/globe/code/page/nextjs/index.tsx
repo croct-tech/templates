9d033fe8d3d8cb0a9450d9e7ae7fbbc2207818f3
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-15 10:23:32 -0300

    Add example for Magic UI globe (#11)

---
+++ templates/magicui/ui/globe/code/page/nextjs/index.tsx
@@ -0,0 +1,33 @@
+import type { GetServerSideProps } from "next"
+import type { SlotContent } from "@croct/plug-next"
+import { GlobalPresence } from "?/**/*/global-presence.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export type PageProps = {
+  content: SlotContent<'%slotId%@%slotVersion%'>
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async context => ({
+  props: await fetchContent('%slotId%@%slotVersion%', {route: context})
+});
+
+export default function Page({content}: PageProps) {
+  return (
+    <TemplateCanvas
+      title="Magic UI - Globe"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <div>
+          <GlobalPresence {...content} />
+        </div>
+      </div>
+    </TemplateCanvas>
+  );
+}
---
+++ templates/magicui/ui/globe/code/page/nextjs/page.tsx
@@ -0,0 +1,25 @@
+import { GlobalPresence } from "?/**/*/global-presence.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export default async function Page() {
+  const {content} = await fetchContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      title="Magic UI - Globe"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <div>
+          <GlobalPresence {...content} />
+        </div>
+      </div>
+    </TemplateCanvas>
+  );
+}

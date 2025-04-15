e641fcf2f67b060d1eb3c7f397411d6437deb2e9
Marcos Passos <marcos@croct.com>
2025-04-15 15:13:55 -0300

    Add template for Magic UI lens

---
+++ templates/magicui/ui/lens/code/page/nextjs/index.tsx
@@ -0,0 +1,32 @@
+import type { GetServerSideProps } from "next"
+import type { SlotContent } from "@croct/plug-next"
+import { LensCard } from "?/**/*/lens-card.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export type PageProps = {
+  content: SlotContent<'%slotId%@%slotVersion%'>
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async context => ({
+  props: await fetchContent('%slotId%@%slotVersion%', {route: context})
+});
+
+export default function Page({content}: PageProps) {
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Lens"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <LensCard {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}
---
+++ templates/magicui/ui/lens/code/page/react/page.tsx
@@ -0,0 +1,24 @@
+import { LensCard } from "?/**/*/lens-card.tsx"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Lens"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <LensCard {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}

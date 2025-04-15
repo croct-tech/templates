5dc849c51762b7874e7aff37c9c8828138d3ec89
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-15 14:43:26 -0300

    Add template for Magic UI icon cloud (#16)

---
+++ templates/magicui/ui/icon-cloud/code/components/icon-cloud-demo.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { IconCloud } from "?/**/*/magicui/icon-cloud.tsx";
+
+interface IconCloudDemoProps {
+  images: string[];
+}
+
+export function IconCloudDemo({images}: IconCloudDemoProps) {
+  return (
+    <div className="relative flex size-full items-center justify-center overflow-hidden">
+      <IconCloud images={images} />
+    </div>
+  );
+}

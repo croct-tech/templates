ad681f8271902a77416749f61280754b9f236587
Marcos Passos <marcos@croct.com>
2025-04-15 12:26:23 -0300

    Add template for Magic UI dock

---
+++ templates/magicui/ui/dock/code/page/nextjs/page.tsx
@@ -0,0 +1,24 @@
+import { DockMenu } from "?/**/*/dock-menu.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export default async function Page() {
+  const {content} = await fetchContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Dock"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <DockMenu {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}
---
+++ templates/magicui/ui/dock/code/page/react/page.tsx
@@ -0,0 +1,24 @@
+import { DockMenu } from "?/**/*/dock-menu.tsx"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Dock"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <DockMenu {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}

1036e56c825c60b0bed967245e57dd92777507ef
Marcos Passos <marcos@croct.com>
2025-04-15 15:34:14 -0300

    Add template for Magic UI script copy btn

---
+++ templates/magicui/ui/script-copy-btn/code/page/nextjs/page.tsx
@@ -0,0 +1,24 @@
+import { CopyCommandBlock } from "?/**/*/copy-command-block.tsx"
+import { TemplateCanvas } from "@croct/template-ui/next"
+import { fetchContent } from "@croct/plug-next/server"
+
+export default async function Page() {
+  const {content} = await fetchContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Script copy button"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <CopyCommandBlock {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}
---
+++ templates/magicui/ui/script-copy-btn/code/page/react/page.tsx
@@ -0,0 +1,24 @@
+import { CopyCommandBlock } from "?/**/*/copy-command-block.tsx"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      theme="light"
+      title="Magic UI - Script copy button"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%"
+      ctaTarget="_blank"
+      src="#"
+      fullScreen
+      portal
+    >
+      <div className="flex absolute h-full w-full items-center justify-center p-10">
+        <CopyCommandBlock {...content} />
+      </div>
+    </TemplateCanvas>
+  );
+}

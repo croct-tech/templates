6e4351fddd6b10adb02f393eacc2223e9fd77b08
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-15 15:35:15 -0300

    Add template for Magic UI script copy btn (#18)

---
+++ templates/magicui/ui/script-copy-btn/code/components/copy-command-block.tsx
@@ -0,0 +1,24 @@
+import { ScriptCopyBtn } from "?/**/*/magicui/script-copy-btn.tsx";
+
+interface Command {
+  tabName: string;
+  command: string
+}
+
+interface CopyCommandBlockProps {
+  codeLanguage: string;
+  commands: Command[],
+  showMultiplePackageOptions?: boolean;
+}
+
+export function CopyCommandBlock({codeLanguage, commands, showMultiplePackageOptions}: CopyCommandBlockProps) {
+  return (
+    <ScriptCopyBtn
+      codeLanguage={codeLanguage}
+      showMultiplePackageOptions={showMultiplePackageOptions}
+      lightTheme="nord"
+      darkTheme="vitesse-dark"
+      commandMap={Object.fromEntries(commands.map(command => [command.tabName, command.command]))}
+    />
+  );
+}

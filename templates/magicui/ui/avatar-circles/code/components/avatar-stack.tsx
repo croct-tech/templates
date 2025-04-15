bd3c965f05935fc9499c1306292e7079bdc65714
Marcos Passos <marcospassos@users.noreply.github.com>
2025-04-15 14:10:11 -0300

    Add template for Magic UI avatar circles (#15)

---
+++ templates/magicui/ui/avatar-circles/code/components/avatar-stack.tsx
@@ -0,0 +1,16 @@
+import { AvatarCircles } from "?/**/*/magicui/avatar-circles.tsx";
+
+interface AvatarProps {
+  imageUrl: string;
+  profileUrl: string;
+}
+
+interface AvatarStackProps {
+  numPeople?: number;
+  avatars: AvatarProps[]
+}
+
+export function AvatarStack({avatars, numPeople}: AvatarStackProps) {
+  return <AvatarCircles avatarUrls={avatars} numPeople={numPeople} />;
+}
+

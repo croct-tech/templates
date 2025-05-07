ecbaca38a8fcb80caf5c5f216e90730a0f570465
Marcos Passos <marcos@croct.com>
2025-05-07 12:35:39 -0300

    Improve announcement bar

--- templates/croct/ui/component/announcement-bar/code/components/announcement-bar.tsx
+++ templates/croct/ui/component/announcement-bar/code/components/announcement-bar.tsx
@@ -1,29 +1,59 @@
 "use client"
 
-import {X} from "lucide-react"
+import {
+  X,
+  MapPin,
+  Sparkles,
+  Gift,
+  Star,
+  TicketPercent,
+  AlarmClock,
+  CircleCheck,
+  Info,
+  CircleAlert,
+  CircleX
+} from "lucide-react"
 import styles from "?/./announcement-bar.module.css"
-import {useState} from "react"
+import {useState, type ElementType, type CSSProperties} from "react"
 import {renderMarkdown} from "?/**/*/markdown.{js,tsx}"
 
-interface AnnouncementBarProps {
-  bar?: {
+type AnnouncementBarProps = {
+  bar ? : {
+    icon? : 'pin' | 'sparkles' | 'gift' | 'star' | 'discount' | 'alarm' | 'check' | 'info' | 'warning' | 'error'
     message: string
-    dismissible?: boolean
-    link?: {
+    dismissible? : boolean
+    link? : {
       url: string
-      label?: string
-      newTab?: boolean
+      label? : string
+      newTab? : boolean
     }
     style: {
-      alignment?: "left" | "center"
-      placement?: "top" | "bottom" | "floating-bottom" | "floating-bottom-center"
-      emphasized?: boolean
+      backgroundColor? : string
+      textColor? : string
+      alignment? : "left" | "center"
+      placement? : "top" | "bottom" | "floating-bottom" | "floating-bottom-center"
+      emphasized? : boolean
     }
   }
 }
 
+const iconMap: Record<string, ElementType> = {
+  pin: MapPin,
+  sparkles: Sparkles,
+  gift: Gift,
+  star: Star,
+  discount: TicketPercent,
+  alarm: AlarmClock,
+  check: CircleCheck,
+  info: Info,
+  warning: CircleAlert,
+  error: CircleX
+};
+
 export function AnnouncementBar(props: AnnouncementBarProps) {
-  if (props.bar == undefined) {
+  const [isVisible, setIsVisible] = useState(true)
+
+  if (props.bar == undefined || !isVisible) {
     return null
   }
 
@@ -31,19 +61,37 @@ export function AnnouncementBar(props: AnnouncementBarProps) {
     message,
     dismissible = true,
     link,
+    icon,
     style: {
+      backgroundColor,
+      textColor,
       alignment = 'left',
       placement = 'top',
       emphasized = false
     }
   } = props.bar
-  const [isVisible, setIsVisible] = useState(true)
-
-  if (!isVisible) {
-    return null
-  }
 
   const target = link?.newTab === true ? "_blank" : undefined
+  const Icon = icon !== undefined ? iconMap[icon] : null
+
+  const content = (
+    <div className={[styles.content, dismissible ? styles.dismissible : ""].join(" ")}>
+      {Icon && (<Icon className={styles.icon} />)}
+      <p className={styles.message}>{renderMarkdown(message)}</p>
+      {link?.label && (
+        <a href={link.url} target={target} className={styles.action}>
+          {link.label}
+        </a>
+      )}
+
+      {dismissible && (
+        <button className={styles['close-button']} onClick={() => setIsVisible(false)}
+                aria-label="Close announcement">
+          <X size={18}/>
+        </button>
+      )}
+    </div>
+  );
 
   const classes = [
     styles['announcement-bar'],
@@ -52,31 +100,18 @@ export function AnnouncementBar(props: AnnouncementBarProps) {
     emphasized ? styles.emphasized : "",
   ].join(" ");
 
-  const content = (
-    <>
-      <div className={[styles.content, dismissible ? styles.dismissible : ""].join(" ")}>
-        <p className={styles.message}>{renderMarkdown(message)}</p>
-        {link?.label && (
-          <a href={link.url} target={target} className={styles.action}>
-            {link.label}
-          </a>
-        )}
-      </div>
-      {dismissible && (
-        <button className={styles['close-button']} onClick={() => setIsVisible(false)} aria-label="Close announcement">
-          <X size={18}/>
-        </button>
-      )}
-    </>
-  );
+  const style = {
+    '--background-color': backgroundColor,
+    '--text-color': textColor,
+  } as CSSProperties;
 
   return link?.label === undefined && link?.url !== undefined
     ? (
-      <a href={link.url} className={classes} target={target}>
+      <a href={link.url} className={classes} target={target} style={style}>
         {content}
       </a>
     ) : (
-      <div className={classes}>
+      <div className={classes} style={style}>
         {content}
       </div>
     )

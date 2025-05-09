33dddea935d7fbc32a082225d080127a7f5981bf
Juliana Amorim <juliana@croct.com>
2025-05-09 17:44:14 -0300

    Fix css

--- templates/npm/react-phone-number-input/code/components/phone-input.tsx
+++ templates/npm/react-phone-number-input/code/components/phone-input.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { default as BasePhoneInput, type DefaultInputComponentProps, type Props } from "react-phone-number-input"
-import "?/./phone-input.css"
+import styles from "?/./phone-input.module.css"
 
 export type PhoneInputProps<P extends DefaultInputComponentProps> = Props<P> | Omit<Props<P>, 'onChange'>;
 
 export function PhoneInput<P extends DefaultInputComponentProps>({onChange, ...props}: PhoneInputProps<P>) {
   return (
     <BasePhoneInput
+      className={styles.PhoneInput}
       onChange={onChange ?? (() => {})}
       {...props}
     />

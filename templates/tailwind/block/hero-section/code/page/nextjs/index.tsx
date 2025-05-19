5cfcbb3dc6f7d5484d864576ec2f4e111a775053
Juliana Amorim <juliana@croct.com>
2025-05-19 17:39:18 -0300

    Add block implementation

---
+++ templates/tailwind/block/hero-section/code/page/nextjs/index.tsx
@@ -0,0 +1,28 @@
+import type { GetServerSideProps } from "next"
+import type { SlotContent } from "@croct/plug-next"
+import { HeroDemo } from "?/**/*/hero-demo.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { fetchContent } from "@croct/plug-next/server"
+
+export type PageProps = {
+  content: SlotContent<'%slotId%@%slotVersion%'>
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async context => ({
+  props: await fetchContent('%slotId%@%slotVersion%', {route: context})
+});
+
+export default function Page({content}: PageProps) {
+  return (
+    <TemplateCanvas
+      title="Tailwind - Hero section"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.tailwind&utm_content=hero_section&utm_term=nextjs"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <HeroDemo hero={content} />
+    </TemplateCanvas>
+  );
+}
---
+++ templates/tailwind/block/hero-section/code/page/nextjs/page.tsx
@@ -0,0 +1,20 @@
+import { HeroDemo } from "?/**/*/hero-demo.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { fetchContent } from "@croct/plug-next/server"
+
+export default async function Page() {
+  const {content} = await fetchContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      title="Tailwind - Hero section"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.tailwind&utm_content=hero_section&utm_term=nextjs"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <HeroDemo hero={content} />
+    </TemplateCanvas>
+  );
+}

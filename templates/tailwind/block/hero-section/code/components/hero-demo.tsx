28017d25a9d95879a1aa384d363666f90725ea93
Marcos Passos <marcos@croct.com>
2025-05-20 11:40:17 -0300

    Fix overflow

--- templates/tailwind/block/hero-section/code/components/hero-demo.tsx
+++ templates/tailwind/block/hero-section/code/components/hero-demo.tsx
@@ -20,7 +20,7 @@ export function HeroDemo(props: HeroDemoProps) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-      <div className="bg-white">
+      <div className="bg-white overflow-hidden">
         <header className="absolute inset-x-0 top-0 z-50">
           <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
             <div className="flex lg:flex-1">

554c8d9d7df07398c753e22bfa426ccd99386163
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-05-22 19:00:43 -0300

    Add Tailwind logo cloud template (#95)

---
+++ templates/tailwind/block/logo-cloud/code/page/react/page.tsx
@@ -0,0 +1,21 @@
+import { LogoCloud } from "?/**/*/logo-cloud.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      title="Tailwind - Logo Cloud"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.tailwind&utm_content=logo_cloud&utm_term=react"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <LogoCloud {...content} />
+    </TemplateCanvas>
+  );
+}
+

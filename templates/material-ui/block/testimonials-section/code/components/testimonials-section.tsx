7b14845f12dcc22addc792690a7e320374584d64
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-05-27 14:42:36 -0300

    Fixes

--- templates/material-ui/block/testimonials-section/code/components/testimonials-section.tsx
+++ templates/material-ui/block/testimonials-section/code/components/testimonials-section.tsx
@@ -9,25 +9,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import { useTheme } from '@mui/system';
-
-const whiteLogos = [
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628e8573c43893fe0ace_Sydney-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d520d0517ae8e8ddf13_Bern-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f46794c159024c1af6d44_Montreal-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e891fa22f89efd7477a_TerraLight.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a09d1f6337b1dfed14ab_colorado-white.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5caa77bf7d69fb78792e_Ankara-white.svg',
-];
-
-const darkLogos = [
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560628889c3bdf1129952dc_Sydney-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f4d4d8b829a89976a419c_Bern-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f467502f091ccb929529d_Montreal-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e911fa22f2203d7514c_TerraDark.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/6560a0990f3717787fd49245_colorado-black.svg',
-  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/655f5ca4e548b0deb1041c33_Ankara-black.svg',
-];
+import { useColorScheme } from "@mui/material/styles";
 
 const logoStyle = {
   width: '64px',
@@ -42,15 +24,18 @@ export type TestimonialsSection = {
     quotee: {
       name: string,
       role: string,
-      avatar: string
+      avatar: string,
+      companyLogo: {
+        darkModeLogo: string,
+        lightModeLogo: string
+      }
     }
   }>
 }
 
 export function TestimonialsSection(props: TestimonialsSection) {
   const {title, description, testimonials} = props;
-  const theme = useTheme();
-  const logos = theme.palette.mode === 'light' ? darkLogos : whiteLogos;
+  const theme = useColorScheme();
 
   return (
     <Container
@@ -69,6 +54,7 @@ export function TestimonialsSection(props: TestimonialsSection) {
         sx={{
           width: { sm: '100%', md: '60%' },
           textAlign: { sm: 'left', md: 'center' },
+          marginTop: "10em",
         }}
       >
         <Typography
@@ -119,7 +105,7 @@ export function TestimonialsSection(props: TestimonialsSection) {
                   subheader={testimonial.quotee.role}
                 />
                 <img
-                  src={logos[index]}
+                  src={theme.mode === 'light' ? testimonial.quotee.companyLogo.lightModeLogo : testimonial.quotee.companyLogo.darkModeLogo}
                   alt={`Logo ${index + 1}`}
                   style={logoStyle}
                 />

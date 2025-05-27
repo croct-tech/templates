5db41b1b04f714ecdad88c6a4e168917f4b9fbc4
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-05-27 17:57:06 -0300

    Add Material UI navbar template

---
+++ templates/material-ui/block/navbar/code/components/navbar.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import MenuItem from '@mui/material/MenuItem';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import { ColorModeIconDropdown } from "@/components/ColorModeIconDropdown"
+import { SitemarkIcon } from "@/components/SitemarkIcon"
+
+export type NavbarProps = {
+  items: Array<{
+    link: string,
+    label: string
+  }>
+  ctas: Array<{
+    link: string,
+    label: string,
+    apperance: 'text' | 'outlined' | 'contained' 
+  }>
+}
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  flexShrink: 0,
+  borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
+  backdropFilter: 'blur(24px)',
+  border: '1px solid',
+  borderColor: (theme.vars || theme).palette.divider,
+  backgroundColor: theme.vars
+    ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
+    : alpha(theme.palette.background.default, 0.4),
+  boxShadow: (theme.vars || theme).shadows[1],
+  padding: '8px 12px',
+}));
+
+export function Navbar(props: NavbarProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const {items, ctas} = props
+
+  return (
+    <AppBar
+      position="fixed"
+      enableColorOnDark
+      sx={{
+        boxShadow: 0,
+        bgcolor: 'transparent',
+        backgroundImage: 'none',
+        mt: 'calc(var(--template-frame-height, 0px) + 28px)',
+      }}
+    >
+      <Container maxWidth="lg">
+        <StyledToolbar variant="dense" disableGutters>
+          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', px: 0 }}>
+            <SitemarkIcon />
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              {items.map((item, index) => (
+                <Button variant="text" color="info" size="small" href={item.link} key={index}>
+                  {item.label}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
+            {ctas.map((cta, index) => (  
+              <Button color="primary" size="small" variant={cta.apperance} href={cta.link} key={index}>
+                {cta.label}
+              </Button>
+            ))}
+            <ColorModeIconDropdown />
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, gap: 1 }}>
+            <ColorModeIconDropdown size="medium" />
+            <IconButton aria-label="Menu button" onClick={toggleDrawer(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="top"
+              open={open}
+              onClose={toggleDrawer(false)}
+              PaperProps={{
+                sx: {
+                  top: 'var(--template-frame-height, 0px)',
+                },
+              }}
+            >
+              <Box sx={{ p: 2, backgroundColor: 'background.default' }}>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                  }}
+                >
+                  <IconButton onClick={toggleDrawer(false)}>
+                    <CloseRoundedIcon />
+                  </IconButton>
+                </Box>
+
+                {items.map((item, index) => (
+                  <MenuItem key={index} href={item.link}>{item.label}</MenuItem>
+                ))}
+              
+                <Divider sx={{ my: 3 }} />
+
+                {ctas.map((cta, index) => (
+                  <MenuItem key={index}>
+                    <Button color="primary" variant={cta.apperance} fullWidth href={cta.link}>
+                      {cta.label}
+                    </Button>
+                  </MenuItem>
+                ))}
+              </Box>
+            </Drawer>
+          </Box>
+        </StyledToolbar>
+      </Container>
+    </AppBar>
+  );
+}
---
+++ templates/material-ui/block/navbar/code/page/react/page.tsx
@@ -0,0 +1,21 @@
+import { NavbarDemo } from "?/**/*/navbar-demo.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      title="Material UI - Navbar"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.material_ui&utm_content=navbar&utm_term=react"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <NavbarDemo {...content} />
+    </TemplateCanvas>
+  );
+}
+

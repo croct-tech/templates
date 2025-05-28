9758826e70906509e81c35c86e2ad69e3ceffa25
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-05-28 11:44:02 -0300

    Add Material UI FAQ section template

---
+++ templates/material-ui/block/faq-section/code/page/react/page.tsx
@@ -0,0 +1,21 @@
+import { FAQDemo } from "?/**/*/faq-demo.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <TemplateCanvas
+      title="Material UI - FAQ section"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.material_ui&utm_content=fac_section&utm_term=react"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <FAQDemo {...content} />
+    </TemplateCanvas>
+  );
+}
+

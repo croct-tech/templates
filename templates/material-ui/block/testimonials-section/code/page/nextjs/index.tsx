a46e7a21a450d1d709f7969d534ad5ad89df44f7
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-05-29 14:47:23 -0300

    Add Material UI testimonials section (#104)

---
+++ templates/material-ui/block/testimonials-section/code/page/nextjs/index.tsx
@@ -0,0 +1,28 @@
+import type { GetServerSideProps } from "next"
+import type { SlotContent } from "@croct/plug-next"
+import { TestimonialsDemo } from "?/**/*/testimonials-demo.{js,jsx,ts,tsx}"
+import { TemplateCanvas } from "@croct/template-ui/react"
+import { fetchContent } from "@croct/plug-next/server"
+
+export type PageProps = {
+  content: SlotContent<'%slotId%@%slotVersion%'>
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async context => ({
+  props: await fetchContent('%slotId%@%slotVersion%', {route: context})
+});
+
+export default function Page({content}: PageProps) {
+  return (
+    <TemplateCanvas
+      title="Material UI - Testimonials section"
+      ctaLabel="Edit this content"
+      ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.material_ui&utm_content=testimonials_section&utm_term=nextjs"
+      ctaTarget="_blank"
+      fullScreen
+      isolated
+    >
+      <TestimonialsDemo {...content} />
+    </TemplateCanvas>
+  );
+}

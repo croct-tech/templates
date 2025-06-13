d247379f022df9dbae598ea618e29dce3755efbc
Juliana Amorim <juliana@croct.com>
2025-06-13 18:39:34 -0300

    Add sign-up side template from Material UI

---
+++ templates/material-ui/block/sign-up-side/code/page/react/page.tsx
@@ -0,0 +1,21 @@
+import { SignUpSection } from "?/**/*/SignUpSection.{js,jsx,ts,tsx}"
+import { LinkButton } from "@croct/template-ui/react"
+import { useContent } from "@croct/plug-react"
+
+export default function Page() {
+  const content = useContent('%slotId%@%slotVersion%');
+
+  return (
+    <>
+      <SignUpSection {...content} />
+      <LinkButton
+        branded
+        label="Edit side content"
+        href="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.material_ui&utm_content=sign_up_section&utm_term=react"
+        target="_blank"
+        position="bottom-right"
+      />
+    </>
+  );
+}
+

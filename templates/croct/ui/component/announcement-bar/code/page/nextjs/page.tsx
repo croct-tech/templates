d97a5dddeaff1354710c83ccf6937c6e2e0871bb
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2025-07-18 14:45:44 -0300

    Fix Croct templates

--- templates/croct/ui/component/announcement-bar/code/page/nextjs/page.tsx
+++ templates/croct/ui/component/announcement-bar/code/page/nextjs/page.tsx
@@ -13,7 +13,6 @@ export default async function Page() {
           ctaLabel="Edit this content"
           ctaLink="%workspaceUrl%/slots/edit/%slotId%/%slotVersion%?utm_medium=cli&utm_source=template&utm_campaign=00000000.CO.DE.ui_component&utm_content=announcement_bar&utm_term=nextjs"
           ctaTarget="_blank"
-          src="#"
           fullScreen
           isolated
       >
